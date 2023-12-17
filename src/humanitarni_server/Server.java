6b2ea1f042ae16ccc310e8961c69bb0d1116fdd0
os20210224 <os20210224@student.fon.bg.ac.rs>
2023-12-17 16:29:07 +0100

    git namesten?

---
+++ src/humanitarni_server/Server.java
@@ -0,0 +1,15 @@
+package humanitarni_server;
+
+public class Server {
+	//	broj porta na kome ce se otvoriti server soket
+	int port = 7259;
+	
+	public static void main(String[] args) {
+		
+		if (args.length > 0) {
+			
+		}
+		
+	}
+	
+}
